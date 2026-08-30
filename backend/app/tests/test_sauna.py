e47de5ecb506cd499ed10a0b12a5ec56673d657f
Kaisa Hakola <kaisahakola00@gmail.com>
2026-08-30 17:37:59 +0300

    Add two tests to sauna route
    Get all saunas and create new sauna.

--- backend/app/tests/test_sauna.py
+++ backend/app/tests/test_sauna.py
@@ -1,16 +1,33 @@
+import pytest
 from app.models.sauna import Sauna
 from app.models.building import Building
 
-def test_get_saunas(db, client):
+@pytest.fixture
+def building(db):
   building = Building(name="Viialan Kartano", address="Viialantie 666")
   db.add(building)
   db.commit()
   db.refresh(building)
 
+  return building
+
+def test_get_saunas(db, client, building):
   sauna = Sauna(name="A1", building_id=building.id)
   db.add(sauna)
   db.commit()
 
   response = client.get("/saunas")
   assert response.status_code == 200
-  assert response.json()["name"] == "A1"
+  assert response.json()[0]["name"] == "A1"
+
+def test_create_building(db, client, building):
+  response = client.post(
+    "/saunas",
+    json={
+      "name": "B2",
+      "building_id": building.id
+    })
+
+  assert response.status_code == 200
+  assert response.json()["name"] == "B2"
+  assert response.json()["building"]["id"]
