7715517fb468aebb5d7ee28590c25fb6f1b3c4b4
Kaisa Hakola <kaisahakola00@gmail.com>
2026-08-30 16:59:19 +0300

    Clean conftest and test_building
    Move database creation and fixtures to conftest for shared use across all test files.

--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -1,8 +1,39 @@
-from sqlalchemy import create_engine
+import pytest
 
-SQL_ALCHEMY_TEST_DATABASE_ENGINE_URL = "sqlite:///./test_database.db"
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.main import app
+from app.routes.building import get_db
+from app.models.base import Base
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test_database.db"
 
 engine = create_engine(
-  SQL_ALCHEMY_TEST_DATABASE_ENGINE_URL,
+  SQLALCHEMY_DATABASE_URL,
   connect_args={"check_same_thread": False}
 )
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+@pytest.fixture
+def client():
+  return TestClient(app)
+
+@pytest.fixture
+def db():
+  Base.metadata.drop_all(bind=engine)
+  Base.metadata.create_all(bind=engine)
+
+  db = TestingSessionLocal()
+
+  def override_get_db():
+    yield db
+
+  app.dependency_overrides[get_db] = override_get_db
+
+  yield db
+
+  db.close()
+  app.dependency_overrides.clear()
--- backend/app/tests/test_building.py
+++ backend/app/tests/test_building.py
@@ -1,41 +1,6 @@
-import pytest
-from app.main import app
-from fastapi.testclient import TestClient
-from app.routes.building import get_db
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from app.models.base import Base
 from app.models.building import Building
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test_database.db"
-
-engine = create_engine(
-  SQLALCHEMY_DATABASE_URL,
-  connect_args={"check_same_thread": False}
-)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-client = TestClient(app)
-
-@pytest.fixture
-def db():
-  Base.metadata.drop_all(bind=engine)
-  Base.metadata.create_all(bind=engine)
-
-  db = TestingSessionLocal()
-
-  def override_get_db():
-      yield db
-
-  app.dependency_overrides[get_db] = override_get_db
-
-  yield db
-
-  db.close()
-  app.dependency_overrides.clear()
-
-def test_get_buildings(db):
+def test_get_buildings(db, client):
   db_building = Building(address="Siurontie 18", name="Siuron Kartano")
   db.add(db_building)
   db.commit()
@@ -45,7 +10,7 @@ def test_get_buildings(db):
   assert response.json()[0]["name"] == "Siuron Kartano"
   assert response.json()[0]["address"] == "Siurontie 18"
 
-def test_create_building(db):
+def test_create_building(db,client):
   response = client.post(
     "/buildings",
     json={
@@ -58,7 +23,7 @@ def test_create_building(db):
   assert response.json()["name"] == "Siuron Kartano"
   assert response.json()["address"] == "Siurontie 25"
 
-def test_get_building_by_id(db):
+def test_get_building_by_id(db, client):
   new_building = client.post(
     "/buildings",
     json={
@@ -75,7 +40,7 @@ def test_get_building_by_id(db):
   assert response.json()["name"] == "Kaukajärven Kartano"
   assert response.json()["address"] == "Kaukajärventie 25"
 
-def test_delete_building(db):
+def test_delete_building(db, client):
   new_building = client.post(
     "/buildings",
     json={
@@ -92,12 +57,12 @@ def test_delete_building(db):
   response_get = client.get(f"/buildings/{building_id}")
   assert response_get.status_code == 404
 
-def test_get_building_not_found(db):
+def test_get_building_not_found(db, client):
   response = client.get("buildings/23424234")
 
   assert response.status_code == 404
 
-def test_delete_building_not_found(db):
+def test_delete_building_not_found(db, client):
   response = client.delete("buildings/76765473")
 
   assert response.status_code == 404
